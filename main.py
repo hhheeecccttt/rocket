c841d5bb181e8b102586c3ddb81addda6baf5e9d
hhheeecccttt <hectorwang41@gmail.com>
2026-02-24 13:04:27 -0500

    Dynamic air density and gravity

--- main.py
+++ main.py
@@ -2,24 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 EARTH_RADIUS = 6371000
+EARTH_MASS = 6e24
 TM_DIFFERENTIAL = 15
+SEA_LEVEL_GRAVITY = 9.8
+GRAVITATIONAL_CONSTANT = 6.67e-11
 g = 9.8
-RHO = 1.2251
+SEA_LEVEL_AIR_DENSITY = 1.2251
+H = 8500
 CD = 0.75
 A = 80
 
-wet_mass = 2214000
-dry_mass = 137000
+wet_mass = 13000
+dry_mass = 4000
 
-max_thrust = 34500000
+rho = 1.225
+
+max_thrust = 250000
 thrust = 0
-burn_time = 6
+burn_time = 65
 burn_rate = (wet_mass - dry_mass) / burn_time
 
 dt = 0.01
-t_max = 2000
+t_max = 1500
 steps = int(t_max / dt)
-final_step = 0
+final_step = steps - 1
 
 time = np.zeros(steps)
 velocity = np.zeros(steps)
@@ -27,6 +33,12 @@ position = np.zeros(steps)
 mass = np.zeros(steps)
 
 mass[0] = wet_mass
+
+def calculateAirDensity(height, i):
+    return SEA_LEVEL_AIR_DENSITY * np.exp(-position[i] / H)
+
+def calculateGravity(height, i):
+    return SEA_LEVEL_GRAVITY * (EARTH_RADIUS / (EARTH_RADIUS + height)) ** 2
 
 for i in range(steps - 2):
     time[i+1] = time[i] + dt
@@ -38,12 +50,15 @@ for i in range(steps - 2):
         mass[i] = dry_mass
         thrust = 0
 
-    drag = 0.5 * RHO * CD * A * velocity[i] ** 2 * np.sign(velocity[i])
+    rho = calculateAirDensity(position[i], i)
+    g = calculateGravity(position[i], i)
+
+    drag = 0.5 * rho * CD * A * velocity[i] ** 2 * np.sign(velocity[i])
 
     acceleration = (thrust - mass[i] * g - drag) / mass[i]
 
     velocity[i+1] = velocity[i] + acceleration * dt
-    position[i+1] = position[i] + velocity[i] * dt
+    position[i+1] = position[i] + velocity[i] * dt + 0.5 * acceleration * dt**2
 
     if position[i+1] < 0:
         final_step = i
